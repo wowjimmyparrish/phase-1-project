a42b022657e66f6ad5fa8aed42b43279ab619c37
James Parrish <jthomasparrishjr@gmail.com>
2022-07-21 17:32:39 -0500

    added searchByBreed function

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function getRandomDog() {
       randoContainer.append(img);
     });
 }
+function searchByBreed() {
+    //clear previous image, if any
+    document.getElementById("breed").replaceChildren();
+    //grab input value
+    const searchInput = document.getElementById("search-input").value;
+    console.log(searchInput);
+    const breed = document.getElementById("breed");
+    //get images by breed
+    fetch(`https://dog.ceo/api/breed/${searchInput}/images`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //console.log(data.message)
